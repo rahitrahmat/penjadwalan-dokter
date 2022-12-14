0123cd4acd8e7a398db42609efd5d9a9e6e96e2b
unknownit <faisalh0508@gmail.com>
2022-12-15 01:44:27 +0700

    Update Library, Search, Datatables

--- data.php
+++ data.php
@@ -17,6 +17,25 @@ if (isset($_SESSION['username'])) {
         <link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css" />
         <link href="http://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" type="text/css" href="style.css" />
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+        <!-- jQuery -->
+        <script type="text/javascript" 
+            src="https://code.jquery.com/jquery-3.5.1.js">
+        </script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <!-- DataTables CSS -->
+        <link rel="stylesheet" href=
+    "https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
+    
+        <!-- DataTables JS -->
+        <script src=
+    "https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js">
+        </script>
+
     </head>
 
     <body>
@@ -43,17 +62,16 @@ if (isset($_SESSION['username'])) {
                 <p>
                 <h1>Hello, <?php echo $_SESSION['username']; ?></h1>
 
-
                 <?php
                 $sql = mysqli_query($conn, "select * from dokter");
 
-                echo "<table width='100%' id='table1'>
+                echo "<table id='table1' class='display' width='100%'>
                         <thead>
                         <tr>
-                        <th>Nama</th>
-                        <th>Jenis Kelamin</th>
-                        <th>Poliklinik</th>
-                        <th>Tindakan</th>
+                            <th>Nama</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Poliklinik</th>
+                            <th>Tindakan</th>
                         </tr>
                         </thead>
                         <tbody>";
@@ -71,50 +89,22 @@ if (isset($_SESSION['username'])) {
                 ?>
             </div>
             <br>
-            <center>
-            <label for="searchBox">Cari : </label>
-            <input type="text" id="searchBox" placeholder="Search for names.." size="50">
-            </center>
             <script>
-            function performSearch() {
-
-            // Declare search string 
-            var filter = searchBox.value.toUpperCase();
-
-            // Loop through first tbody's rows
-            for (var rowI = 0; rowI < trs.length; rowI++) {
-
-            // define the row's cells
-            var tds = trs[rowI].getElementsByTagName("td");
-
-            // hide the row
-            trs[rowI].style.display = "none";
-
-            // loop through row cells
-            for (var cellI = 0; cellI < tds.length; cellI++) {
-
-                // if there's a match
-                if (tds[cellI].innerHTML.toUpperCase().indexOf(filter) > -1) {
-
-                // show the row
-                trs[rowI].style.display = "";
-
-                // skip to the next row
-                continue;
-
-                }
-            }
-            }
-
-            }
-
-            // declare elements
-            const searchBox = document.getElementById('searchBox');
-            const table = document.getElementById("table1");
-            const trs = table.tBodies[0].getElementsByTagName("tr");
-
-            // add event listener to search box
-            searchBox.addEventListener('keyup', performSearch);
+            // var table = $('#example').DataTable();
+ 
+            // // #myInput is a <input type="text"> element
+            // $('#myInput').on( 'keyup', function () {
+            //     table.search( this.value ).draw();
+            // } );
+            // Initialize the DataTable
+            $(document).ready(function () {
+                $('#table1').DataTable({
+    
+                    // Disable the searching 
+                    // of the DataTable
+                    searching: true
+                });
+            });
             </script>
     </body>
 
